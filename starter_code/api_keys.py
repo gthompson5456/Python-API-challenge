0ccdb1a67ebad38fd205d816923b6da91de2f0f5
gthompson5456 <65741958+gthompson5456@users.noreply.github.com>
2024-08-27 20:18:16 -0400

    Update api_keys.py

--- starter_code/api_keys.py
+++ starter_code/api_keys.py
@@ -1,5 +1,5 @@
-# OpenWeatherMap API Key
+#OpenWeatherMap API Key
 weather_api_key = "ed9ad79c2a99a655b66441e252a621a7"
 
-# Google API Key
+#Google API Key
 g_key = "AIzaSyB5SacOKiTbKFg9va9tRCSwSyySEJZ48ag"
